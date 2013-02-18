f8d5d45a93aebf89712ea6c210afebf1e4c5b248
Charlie Brown <carbonrobot@gmail.com>
2013-02-18 11:59:32 -0600

    Add radio list and span tags

---
+++ source/Tagger/RadioList.cs
@@ -0,0 +1,33 @@
+﻿namespace Tagger
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class RadioList : Span
+    {
+        public RadioList()
+        {
+        }
+
+        public RadioList AddOptions(string name, IEnumerable<KeyValuePair<string, string>> options, string selectedValue = "", string optionCssClass = "", string labelCssClass = "")
+        {
+            options.ToList().ForEach(kvp =>
+            {
+                var option = new Radio()
+                    .Text(kvp.Value, labelCssClass)
+                    .Name(name)
+                    .Value(kvp.Key)
+                    .Id(string.Format("{0}_{1}", name, kvp.Key))
+                    .Class(optionCssClass);
+
+                if (selectedValue == kvp.Key)
+                    ((Radio)option).Checked();
+
+                this.Add(option);
+            });
+
+            return this;
+        }
+
+    }
+}
---
+++ source/Tagger/Span.cs
@@ -0,0 +1,9 @@
+﻿namespace Tagger
+{
+    public class Span : TextTag
+    {
+        public Span() : base("span")
+        {
+        }
+    }
+}
