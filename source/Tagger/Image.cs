596dc803cabe06c7bd1b5a20463f2741fd07f1b4
Charlie Brown <carbonrobot@gmail.com>
2013-02-18 11:30:38 -0600

    Added image tag

---
+++ source/Tagger/Image.cs
@@ -0,0 +1,14 @@
+﻿namespace Tagger
+{
+    public class Image : Tag<Image>
+    {
+        public Image() : base("img")
+        {
+        }
+
+        public Image Src(string value)
+        {
+            return this.Attribute(Attributes.Source, value);
+        }
+    }
+}
