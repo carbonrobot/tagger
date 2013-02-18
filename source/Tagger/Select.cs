d08cb053ee348853682fdac56def66a727817f00
Charlie Brown <carbonrobot@gmail.com>
2013-02-18 11:49:18 -0600

    Add enumerable options to select

--- source/Tagger/Select.cs
+++ source/Tagger/Select.cs
@@ -1,5 +1,7 @@
 ﻿namespace Tagger
 {
+    using System.Collections.Generic;
+    using System.Linq;
 
     /// <summary>
     /// Select Element
@@ -12,6 +14,21 @@
         public Select()
             : base("select")
         {
+        }
+
+        public Select AddOptions(IEnumerable<KeyValuePair<string, string>> options, string selectedValue = "")
+        {
+            options.ToList().ForEach(kvp =>
+            {
+                var option = new Option()
+                    .Value(kvp.Key)
+                    .Text(kvp.Value);
+                if (selectedValue == kvp.Key)
+                    ((Option)option).Selected();
+
+                this.Add(option);
+            });
+            return this;
         }
 
     }
