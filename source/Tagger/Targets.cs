b56272abe3a38f6a65d39ea51192a8b2cf557aff
Charlie Brown <carbonrobot@gmail.com>
2013-02-18 11:48:27 -0600

    Add anchor targets

---
+++ source/Tagger/Targets.cs
@@ -0,0 +1,10 @@
+﻿namespace Tagger
+{
+    public static class Targets
+    {
+        public const string Self = "_self";
+        public const string Blank = "_blank";
+        public const string Parent = "_parent";
+        public const string Top = "_top";
+    }
+}
