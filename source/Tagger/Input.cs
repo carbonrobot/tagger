c3940d180e5f39db35f3d5831b5e2ba3e0b42c52
Charlie Brown <carbonrobot@gmail.com>
2013-02-18 11:29:18 -0600

    Removed doc comments

--- source/Tagger/Input.cs
+++ source/Tagger/Input.cs
@@ -1,19 +1,8 @@
 ﻿namespace Tagger
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    
-    /// <summary>
-    /// Input element
-    /// </summary>
     public class Input : FormElement
     {
-        /// <summary>
-        /// Constructs a new input tag
-        /// </summary>
-        /// <param name="type">The type of input</param>
+        
         public Input(string type)
             : base("input")
         {
